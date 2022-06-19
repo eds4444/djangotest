ccaf7bf036ceb4a4e3a6e785927f059b030b02f4
eds4444 <efimov0204@gmail.com>
2022-06-19 22:30:07 +0300

    first commit

---
+++ women/views.py
@@ -0,0 +1,12 @@
+from http.client import HTTPResponse
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def index(request): #HttpRequest
+    return HttpResponse("Страница приложения women")
+
+def categories(request): #HttpRequest
+    return HttpResponse("<h1>Статьи по категориям</h1>")
+
+
