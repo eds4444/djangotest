70a67ed629529e44acf25df124466214e31fd307
eds4444 <efimov0204@gmail.com>
2022-06-21 18:15:19 +0300

    GET

--- women/views.py
+++ women/views.py
@@ -1,4 +1,5 @@
 from http.client import HTTPResponse
+from pickle import GET
 from django.http import HttpResponse
 from django.shortcuts import render
 
@@ -6,7 +7,12 @@ from django.shortcuts import render
 def index(request): #HttpRequest
     return HttpResponse("Страница приложения women")
 
-def categories(request, cat): #HttpRequest
-    return HttpResponse(f"<h1>Статьи по категориям</h1><p>{cat}</p>") 
+def categories(request, catid):
+    if request. GET: 
+        print(request. GET)
+        
+    return HttpResponse(f"<h1>Статьи по категориям</h1><p>{catid}</p>") 
 
+def archive(request, year): 
+    return HttpResponse(f"<h1>Архив по годам</h1><p>{year}</p>") 
 
