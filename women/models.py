c443955f0d450d9ac5ce33ca46e2836d40023602
eds4444 <efimov0204@gmail.com>
2022-06-29 18:42:17 +0300

    models

--- women/models.py
+++ women/models.py
@@ -10,3 +10,6 @@ class Women(models.Model):  #наследуем от базового класс
     time_create = models.DateTimeField(auto_now_add=True)
     time_update = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.title
