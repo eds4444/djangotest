7e5f7461a2f94f8e14e4ae1cdbd2327fb813392c
eds4444 <efimov0204@gmail.com>
2022-07-22 19:28:26 +0300

    admin

--- women/models.py
+++ women/models.py
@@ -5,25 +5,38 @@ from django.urls import reverse
 
 
 class Women(models.Model):  #наследуем от базового класса Model (поле id прописанно автоматически в этом классе)
-    title = models.CharField(max_length=250) #https://djbook.ru/rel3.0/   Модули, типы полей.
-    content = models.TextField(blank=True)
-    photo = models.ImageField(upload_to="photos/%Y/%m/%d/")
-    time_create = models.DateTimeField(auto_now_add=True)
-    time_update = models.DateTimeField(auto_now=True)
-    is_published = models.BooleanField(default=True)
-    cat = models.ForeignKey('Category', on_delete=models.PROTECT, null=True)
+    title = models.CharField(max_length=250, verbose_name="Загловок") #https://djbook.ru/rel3.0/   Модули, типы полей.
+    content = models.TextField(blank=True, verbose_name="Текст статьи")
+    photo = models.ImageField(upload_to="photos/%Y/%m/%d/", verbose_name="Фото")
+    time_create = models.DateTimeField(auto_now_add=True, verbose_name="Время создания")
+    time_update = models.DateTimeField(auto_now=True, verbose_name="Время изменения")
+    is_published = models.BooleanField(default=True, verbose_name="Публикация")
+    cat = models.ForeignKey('Category', on_delete=models.PROTECT, null=True, verbose_name="Категория")
 
     def __str__(self):
         return self.title
 
-    def get_absolute_urel(self):
+    def get_absolute_url(self):
         return reverse('post', kwargs={'post_id':self.pk})
 
+    class Meta:
+        verbose_name = 'Известные женщины'
+        verbose_name_plural = 'Известные женщины'
+        ordering = ['-time_create', 'title']
+
+
 class Category(models.Model):
-    name = models.CharField(max_length=100, db_index=True)
+    name = models.CharField(max_length=100, db_index=True, verbose_name = "Категория")
 
     def __str__(self):
         return self.name
 
     def get_absolute_urel(self):
         return reverse('category', kwargs={'cat_id':self.pk})
+
+    class Meta:
+        verbose_name = 'категорию'
+        verbose_name_plural = 'Категории'
+        ordering = ['id']
+
+        
